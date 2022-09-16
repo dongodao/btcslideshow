938f5425e8a8fd8328255f2951adb6c272c2c43b
dongodao <dongodao@hotmail.com>
2022-09-16 17:29:55 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,40 +1,42 @@
-
 // Copyright 2019-2022 GPLv3, Slideshow Crypto Ticker by Mike Kilday: http://DragonFrugal.com
 
-var exchange_markets = []; // LEAVE ALONE, AND DON'T DELETE (REQUIRED!)
+var exchange_markets = []; // LEAVE ALONE, AND DON'T DELETE (REQUIRED!!)
 
+
+///////////////////////////////////////////
+// START config
+///////////////////////////////////////////
 
 //  TYPOS LIKE MISSED SEMICOLONS / MISSED QUOTES / ETC WILL BREAK THE APP, BE CAREFUL EDITING THIS CONFIG FILE!
 
 
 // ############## PER-EXCHANGE FORMATTING EXAMPLES ##########################
 ////
-// Which crypto asset(s) to display on the ticker
-// Separate with pipe | symbol to "slideshow" between multiple tickers on the same exchange
+// YOU CAN RE-ORDER THE EXCHANGES IN THE CONFIG, TO RE-ORDER WHICH EXCHANGE'S MARKET'S SHOW FIRST
 ////
-// Bitstamp formatting example  (MULTIPLE TICKERS NOT SUPPORTED): 'btcgbp'
+// Which crypto asset(s) to display on the ticker
+// SEPERATE MULTIPLE EXCHANGE'S MARKETS with the pipe | symbol to "slideshow" between multiple tickers on the same exchange
+////
+// Bitfinex formatting example  (MULTIPLE TICKERS NOT SUPPORTED): 'BTCEUR', OR 'PLANETS:UST' #FOR OVER 4 CHARACTER SYMBOLS#
 
-// Bitfinex formatting example  (MULTIPLE TICKERS NOT SUPPORTED): 'BTCEUR', OR 'PLANETS:UST' FOR OVER 4 CHARACTER SYMBOLS
-
-// Hitbtc formatting example    (MULTIPLE TICKERS NOT SUPPORTED): 'MYSTBTC'
+// COINGECKO NOTES: 'API_ID_HERE:ASSET/PAIR' ('API id' is on the asset's coingecko page, in the 'Info' section)
+// CoinGecko formatting example: 'bitcoin:btc/usd|bitcoin:btc/eur|bitcoin:btc/chf|grape-2:grape/usd|raydium:ray/btc'
 
 // Coinbase formatting example: 'BTC-USD|BTC-GBP|ETH-USD|ETH-BTC|ETH-EUR|MKR-USD|MKR-BTC|MANA-USDC'
 
-// Binance formatting example:  'btcusdt|ethusdt|ethbtc|mkrusdt'
-
-// CoinGecko formatting example: 'bitcoin:btc/usd|bitcoin:btc/eur|bitcoin:btc/chf|grape-2:grape/usd|invictus:in/usd|raydium:ray/btc'
-
-// Loopring formatting example: 'LRC-USDT|LRC-ETH'
-
 // Kraken formatting example:   'XBT/USD|XBT/CAD|XBT/EUR|ETH/USD|ETH/EUR|ETH/CAD'
+
+// Hitbtc formatting example    (MULTIPLE TICKERS NOT SUPPORTED): 'ETHBTC'
+
+// Bitstamp formatting example  (MULTIPLE TICKERS NOT SUPPORTED): 'btcgbp'
+
+// Binance formatting example:  'btcusdt|ethusdt|ethbtc|mkrusdt'
 
 // Kucoin formatting example:   'MANA-USDT|ENJ-BTC'
 
 // Okex formatting example:     'ENJ-USDT|ENJ-BTC'
 
 // Gate.io formatting example:  'MANA_USDT|SAMO_USDT'
-
-// Bitmart formatting example:  'SG_USDT|SG_BTC'
 ////
 //// 
 // Bitstamp markets (set to '' to disable)
@@ -49,31 +51,15 @@ exchange_markets['bitfinex'] = '';
 ////
 // Coinbase markets (set to '' to disable)
 exchange_markets['coinbase'] = ''; 
-////
+////exchange_markets['coinbase'] = 'ETH-USD|ETH-BTC|SOL-USD'; 
+
 //// 
 // Coingecko markets (set to '' to disable)
 // USE COINGECKO'S API ID FOR EACH ASSET! (SEE COINGECKO ASSET PAGE'S INFO SECTION) 
 // PAIRING ASSET MUST BE SUPPORTED BY COINGECKO'S 'vs_currencies' API PARAMETER!
 // FORMAT IS 'api-id-here:symbol/pairing'
 exchange_markets['coingecko'] = '';
-////
-////
-// Binance markets (set to '' to disable)
-exchange_markets['binance'] = 'ethusdt'; 
-////
-////
-// Loopring markets (set to '' to disable) 
-exchange_markets['loopring'] = '';
-////
-////
-// Kraken markets (set to '' to disable)
-exchange_markets['kraken'] = ''; 
-////
-////
-// Kucoin markets (set to '' to disable)
-// !!KUCOIN REQUIRES USING THE INSTALL SCRIPT!!
-exchange_markets['kucoin'] = ''; 
-////
+////exchange_markets['coingecko'] = 'solana:sol/btc|solana:sol/eth|genesysgo-shadow:shdw/usd|wrapped-cusd-allbridge-from-celo:acusd/usd';
 ////
 // OKex markets (set to '' to disable) 
 exchange_markets['okex'] = '';
@@ -84,12 +70,21 @@ exchange_markets['okex'] = '';
 exchange_markets['hitbtc'] = ''; 
 ////
 ////
-// Bitmart markets (set to '' to disable) 
+// Kucoin markets (set to '' to disable)
+// !!KUCOIN REQUIRES USING THE INSTALL SCRIPT!!
+exchange_markets['kucoin'] = ''; 
+////
+////
+// Kraken markets (set to '' to disable)
+exchange_markets['kraken'] = ''; 
+////
+////
+// Binance markets (set to '' to disable)
+exchange_markets['binance'] = 'btcusdt'; 
+////
+////
+// Gateio markets (set to '' to disable) 
 exchange_markets['gateio'] = '';
-////
-////
-// Bitmart markets (set to '' to disable) 
-exchange_markets['bitmart'] = '';
 
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -99,17 +94,17 @@ exchange_markets['bitmart'] = '';
 
 
 // Screen orientation (offset in degrees)
-var orient_screen = 180; // Normal (upright) = 0, Flipped (upside down) = 180, Sideways (left or right) = 90 or 270
+var orient_screen = 0; // Normal (upright) = 0, Flipped (upside down) = 180, Sideways (left or right) = 90 or 270
 
 
 // Vertical position (adjusts the ticker's vertical position up/down)
 // CAN BE NEGATIVE, TO GO THE OPPOSITE WAY
-var vertical_position = 3; // Default = 35 (SMALL SCREEN), 70 (LARGE SCREEN)
+var vertical_position = 3; // Default = 37 (SMALL SCREEN), 75 (MEDIUM SCREEN), 150 (LARGE SCREEN)
 
 
 // Horizontal position (adjusts the ticker's horizontal position left/right)
 // CAN BE NEGATIVE, TO GO THE OPPOSITE WAY
-var horizontal_position = 0; // Default = 10 (SMALL SCREEN), 0 (LARGE SCREEN)
+var horizontal_position = 0; // Default = 10 (SMALL SCREEN), 0 (MEDIUM/LARGE SCREEN)
 
 
 // Show exchange name in title (next to asset ticker symbol)
@@ -120,44 +115,67 @@ var show_exchange_name = 'on'; // 'on' / 'off', Default = 'on'
 var show_empty_volume = 'on'; // 'off' / 'on', Default = 'on'
 
 
-// SECONDS between "slideshowing" multiple tickers (if multiple assets configured)
-// SET TO 0 FOR AUTO MODE (trys to show all tickers in 1 minute, BUT has a 5 second per-ticker MINIMUM)
-var slideshow_speed = 4; // Default = 4
-
-
-// Slideshow transition speed (fade out / fade in) IN SECONDS (can be decimals)
-var transition_speed = 0.75; // Default = 0.75
-
-
-// ALL font weights (for ALL ticker text)
-var font_weight = 'normal'; // Default = '100', can be any proper CSS font weight value, can be any proper CSS font weight value 100-900, 400 is the same as normal, and 700 is the same as bold
+// Show system temperature / memory stats (TOP LEFT corner of app screen, if device temps / RAM useage are available)
+var show_system_data = 'on'; // 'on' / 'off', Default = 'on'
+////
+// System data font size
+var system_data_size = 3.00; // Default = 3.00 (#CAN# BE DECIMALS HERE, AS WERE USING THE CSS vw STANDARD)
 
 
 // Title font size
-var title_size = 45; // Default = 57 (SMALL SCREEN), 115 (LARGE SCREEN)
-
-
-// Ticker arrow size ratio (to ticker size), DECIMAL NUMBER FORMAT X.XX OF 1.00 OR LESS
-// THIS #ALREADY AUTO-RESIZES# BASED ON THE TICKER SIZE, SO YOU USUALLY CAN LEAVE THIS
-// #AS-IS#, UNLESS YOU WANT THE RATIO TO TICKER SIZE DIFFERENT!
-var arrow_size = 0.65; // Default = 0.65
-
-
-// Ticker font size
-var ticker_size = 95; // Default = 75 (SMALL SCREEN), 150 (LARGE SCREEN)
+var title_size = 45; // Default = 57 (SMALL SCREEN), 115 (MEDIUM SCREEN), 230 (LARGE SCREEN)
 
 
 // 24 hour volume font size
-var volume_size = 35; // Default = 42 (SMALL SCREEN), 84 (LARGE SCREEN)
+var volume_size = 35; // Default = 42 (SMALL SCREEN), 84 (MEDIUM SCREEN), 168 (LARGE SCREEN)
+
+
+// Ticker font size
+var ticker_size = 95; // Default = 80 (SMALL SCREEN), 160 (MEDIUM SCREEN), 320 (LARGE SCREEN)
+
+
+// Ticker arrow size RATIO (to ticker size), DECIMAL NUMBER FORMAT X.XX OF 1.00 OR LESS
+// THIS #ALREADY AUTO-RESIZES# BASED ON THE TICKER SIZE, SO YOU USUALLY CAN LEAVE THIS
+// #AS-IS#, UNLESS YOU WANT THE RATIO TO TICKER SIZE DIFFERENT!
+var arrow_size = 0.65; // Default = 0.65 (same height as ticker price text)
+////
+// Spacing between ticker arrow, and ticker price text
+var arrow_spacing = 20; // Default = 10 (SMALL SCREEN), 20 (MEDIUM SCREEN), 40 (LARGE SCREEN)
 
 
 // Maximum decimal places for ticker values worth under 1.00 in unit value, for prettier / less-cluttered interface
-var max_ticker_decimals = 2; // Default = 6
+var ticker_max_decimals = 2; // Default = 6
 
 
-// Minimum decimal places for ANY ticker values
+// Minimum decimal places for ANY ticker values,
+// EXCEPT FOR FIAT UNIT VALUES UNDER 100 (which are forced to 2 decimals minimum, UNLESS THIS VALUE IS 2 OR HIGHER)
 // 0 disables
-var min_ticker_decimals = 0; // Default = 0
+var ticker_min_decimals = 0; // Default = 0
+
+
+// PRICE PERCENTAGE to round off ticker price IN DECIMALS (DYNAMIC / RELATIVE to price amount)
+// (FINE-GRAINED CONTROL OVER TICKER PRICE ROUNDING #AMOUNT OF DECIMALS SHOWN#)
+// (interface examples: one = 1000, tenth = 1000, hundredth = 1000.9, thousandth = 1000.09)
+// (interface examples: one = 100, tenth = 100.9, hundredth = 100.09, thousandth = 100.009)
+// (interface examples: one = 10.9, tenth = 10.09, hundredth = 10.009, thousandth = 10.0009)
+// #FIAT# CURRENCY VALUES UNDER 100 #ARE FORCED TO 2 DECIMALS MINUMUM#, UNLESS USING 'thousandth'
+// #ALWAYS# OVERIDDEN BY ticker_min_decimals!
+var ticker_round_percent = 'tenth'; // (OF A PERCENT) 'one', 'tenth', 'hundredth', 'thousandth'
+////
+// FORCE a FIXED MINIMUM amount of decimals on ticker price, CALCULATED OFF ABOVE ticker_round_percent SETTING
+// (ALWAYS SAME AMOUNT OF DECIMALS, #EVEN IF IT INCLUDES TRAILING ZEROS#) 
+// #ALWAYS# OVERIDDEN BY ticker_min_decimals!
+var ticker_round_fixed_decimals = 'on'; // 'off', 'on'
+
+
+// EMULATED / dynamic monospace font WIDTH spacing (percent of EACH font size as a decimal) 
+// (ALL font widths for ticker/volume numbers are emulated as monospace, so numbers don't "jump around" when changing in real-time)
+// DECIMAL NUMBER FORMAT X.XX OF 1.00 OR LESS, OR null to skip (null MUST BE LOWERCASE WITHOUT QUOTES)
+var monospace_width = 0.65; // Default = 0.65
+
+
+// ALL font weights (for ALL ticker text)
+var font_weight = 'normal'; // Default = 'normal', can be any proper CSS font weight value
 
 
 // Text color (https://www.w3schools.com/colors/colors_picker.asp)
@@ -176,20 +194,23 @@ var background_color = '#000000'; // Default = '#000000'
 var google_font = 'Ranchers'; // Default = 'Varela Round'
 
 
-// EMULATED / dynamic monospace font WIDTH spacing (percent of EACH font size as a decimal) 
-// (ALL font widths for ticker/volume numbers are emulated as monospace, so numbers don't "jump around" when changing in real-time)
-// DECIMAL NUMBER FORMAT X.XX OF 1.00 OR LESS, OR null to skip (null MUST BE LOWERCASE WITHOUT QUOTES)
-var monospace_width = 0.65; // Default = 0.65
+// MINUTES before refreshing non-websocket trading exchange API endpoints (coingecko)
+// #DON'T ADJUST TOO LOW#, OR NON-WEBSOCKET APIs LIKE COINGECKO MAY DROP YOUR CONNECTION!
+var rest_api_refresh = 5; // Default = 5
+
+
+// SECONDS between "slideshowing" multiple tickers (if multiple assets configured)
+// SET TO 0 FOR AUTO MODE (trys to show all tickers in 1 minute, BUT has a 5 second per-ticker MINIMUM)
+var slideshow_speed = 9; // Default = 9
+
+
+// Slideshow transition speed (fade out / fade in) IN SECONDS (can be decimals)
+var transition_speed = 0.70; // Default = 0.70
 
 
 // SECONDS to wait before reloading app after internet outages
 // #DON'T ADJUST TOO LOW#, OR DEVICE'S INTERNET MAY NOT BE #FULLY / STABALY# BACK ONLINE YET!
 var app_reload_wait = 60; // Default = 60
-
-
-// MINUTES before refreshing non-websocket API endpoints (coingecko)
-// #DON'T ADJUST TOO LOW#, OR NON-WEBSOCKET APIs LIKE COINGECKO MAY DROP YOUR CONNECTION!
-var rest_api_refresh = 5; // Default = 5
 
 
 // Minimum number of MINUTES to wait before auto-reloading the app IF ERRORS ARE DETECTED THAT MAY BE FIXED WITH A RELOAD
@@ -198,7 +219,12 @@ var rest_api_refresh = 5; // Default = 5
 var auto_error_fix_min = 5; // Default = 5
 
 
-// Enable / disable upgrade check / notice (top right corner of app screen, when an newer version is available for download)
+// HOURS before re-checking for any newly-released upgrades (at github)
+// #KEEP LOW#, AS THIS APP MAY RELOAD ITSELF DAILY IF IT DETECTS API / INTERNET ISSUES,
+// IN WHICH CASE #THE CHECK INTERVAL START TIME IS RESET#
+var upgrade_api_refresh = 1; // Default = 1
+////
+// Enable / disable upgrade check / notice (TOP RIGHT corner of app screen, when an newer version is available for download)
 // (Checks latest release version via github.com API endpoint value "tag_name" 
 // @ https://api.github.com/repos/taoteh1221/Slideshow_Crypto_Ticker/releases/latest)
 var upgrade_notice = 'on'; // 'on' / 'off', Default = 'on'
@@ -206,6 +232,7 @@ var upgrade_notice = 'on'; // 'on' / 'off', Default = 'on'
 
 // DEBUG MODE (turns on console logging for certain logic)
 var debug_mode = 'off'; // 'on' / 'off', Default = 'off'
+
 
 
 // All #ACTIVATED# market pairings (what the asset is paired with in markets), AND their currency symbols
@@ -298,6 +325,7 @@ var fiat_pairings = {
 						'USD': '$',
 						'USDC': 'Ⓢ ',
 						'USDT': '$ ',
+						//'USDT': '₮ ',
 						'UST': 'Ⓢ ',
 						'UYU': '$U ',
 						'VND': '₫',
@@ -309,6 +337,10 @@ var fiat_pairings = {
 					};
 
 
+
+///////////////////////////////////////////
+// END config
+///////////////////////////////////////////
 
 
 
