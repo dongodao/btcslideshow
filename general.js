3d129bd91c2ee252626ead076a147c0fdc4185d2
dongodao <dongodao@hotmail.com>
2022-12-23 19:51:19 -0600

    Update general.js

--- general.js
+++ general.js
@@ -517,7 +517,7 @@ market_key = js_safe_key(market_id, exchange);
 
         html = '<div id="wrapper_' + market_key + '" class="asset_tickers">'+
 
-        '<div class="title" style="font-size: '+title_size+'px; color: #b37f00; font-weight: '+font_weight+';"><span id="asset_' + market_key + '">' + asset +
+        '<div class="title" style="font-size: '+title_size+'px; color: #FFD700; font-weight: '+font_weight+';"><span id="asset_' + market_key + '">' + asset +
         '</span> <span class="status_wrapper_'+exchange+'"><span class="parenth_'+market_key+'">(<span class="status status_'+exchange+' status_'+market_key+'">Loading</span>)</span></span></div>'+
 
         '<div class="ticker" style="font-size: '+ticker_size+'px; color: #09c; font-weight: '+font_weight+';"><span id="ticker_' + market_key + '"></span>'+
